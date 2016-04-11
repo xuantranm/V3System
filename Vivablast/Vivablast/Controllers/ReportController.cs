08f9e898e50e7454e5464c4d9aa0ef634d427d7d
Xuan Tran Minh <minhxuan@ringier.com.vn>
2016-04-11 17:39:08 +0700

    dynamic report

--- Vivablast/Vivablast/Controllers/ReportController.cs
+++ Vivablast/Vivablast/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
-﻿using Ap.Service.Seedworks;
+﻿using Ap.Business.ViewModels;
+using Ap.Service.Seedworks;
 using System;
 using System.Web.Mvc;
 using log4net;
@@ -24,25 +25,11 @@ namespace Vivablast.Controllers
             return View();
         }
 
-        public ActionResult LoadDynamic(int page, int size, string stockCode, string stockName, string store, int type, int category, string enable)
+        public ActionResult LoadDynamic(int page, int size, int poType, string po, int stockType, int category, string stockCode, string stockName)
         {
             var userName = System.Web.HttpContext.Current.User.Identity.Name;
-            var totalRecord = _reportService.ListConditionCount(page, size, stockCode, stockName, store, type, category, enable);
-            var totalTemp = Convert.ToDecimal(totalRecord) / Convert.ToDecimal(size);
-            int totalPages = size == 1000 ? 1 : Convert.ToInt32(Math.Ceiling(totalTemp));
-
-            var model = new StockViewModel
-            {
-                UserLogin = _systemService.GetUserAndRole(0, userName),
-                StockVs = _reportService.ListCondition(page, size, stockCode, stockName, store, type, category, enable),
-                StoreVs = _systemService.StoreList(),
-                TotalRecords = Convert.ToInt32(totalRecord),
-                TotalPages = totalPages,
-                CurrentPage = page,
-                PageSize = size
-            };
-
-            return PartialView("_StockPartial", model);
+            var model = _systemService.GetDynamicReport(page,size,poType,po,stockType,category,stockCode,stockName);
+           return PartialView("Partials/_DynamicReport", model);
         }
     }
 }
